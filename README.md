bac824375f57aaf22f30f8d5620808fca7117db9
aalvarezv <alvarez.vargas@gmail.com>
2022-09-09 22:49:22 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-uvnccq-wwbzyz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-uvnccq-wwbzyz)
---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NyanComponent } from './nyan/nyan.component';
+
+@NgModule({
+  declarations: [AppComponent, HomeComponent, NyanComponent],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: HomeComponent,
+        pathMatch: 'full'
+      },
+      {
+        path: 'nyan',
+        component: NyanComponent
+      }
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
